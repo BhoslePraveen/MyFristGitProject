15b71a7d9209dfd74a5341f37c2eb47850975f25
praveen <praveen.bhosle5@gmail.com>
2023-07-11 21:39:46 +0530

    Added new code

---
+++ src/com/velocity/demo/Example.java
@@ -0,0 +1,11 @@
+package com.velocity.demo;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to git session-Phase-1");
+
+	}
+
+}
---
+++ src/com/velocity/hdfc/HdfcWelcome.java
@@ -0,0 +1,11 @@
+package com.velocity.hdfc;
+
+public class HdfcWelcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		System.out.println("Welcome to HDFC bank");
+
+	}
+
+}
