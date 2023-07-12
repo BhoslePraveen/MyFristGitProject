10dde1693c7990d2fb455a3cc8fe2f556decfe5d
praveen <praveen.bhosle5@gmail.com>
2023-07-12 22:33:05 +0530

    added code for arraylist

---
+++ src/com/velocity/hdfc/Demo.java
@@ -0,0 +1,17 @@
+package com.velocity.hdfc;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add(25);
+		al.add("vishal");
+		al.add(null);
+		al.add(null);
+		System.out.println(al);
+
+	}
+
+}
