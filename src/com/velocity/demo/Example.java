3cfd8c34916f15aacd6704150f2c72ec0046e804
Vishal Deshmukh <vishaldeshmukh963@gmail.com>
2023-07-21 21:58:04 +0530

    Some Code Injection

--- src/com/velocity/demo/Example.java
+++ src/com/velocity/demo/Example.java
@@ -5,7 +5,8 @@ public class Example {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to git session-Phase-1");
-
+		System.out.println("Hello, Velocity...");
+		
 	}
 
 }
