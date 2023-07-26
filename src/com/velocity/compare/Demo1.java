bfe4fd2653336af3096b187966765f36e0b332ef
praveen <praveen.bhosle5@gmail.com>
2023-07-26 21:52:56 +0530

    new changes

---
+++ src/com/velocity/compare/Demo1.java
@@ -0,0 +1,14 @@
+package com.velocity.compare;
+
+import java.util.HashMap;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> map = new HashMap<>();
+		map.put(12, "Praveen");
+		map.put(13, "Vindod");
+
+	}
+
+}
