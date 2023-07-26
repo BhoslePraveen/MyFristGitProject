a2272dcc9868e215c102d49fbe581bf56907a0ff
praveen <praveen.bhosle5@gmail.com>
2023-07-26 22:09:59 +0530

    new change

--- src/com/velocity/compare/Example.java
+++ src/com/velocity/compare/Example.java
@@ -21,11 +21,8 @@ public class Example {
 		list.add(new Employee(101, "Ajay", "ADMIN"));
 		list.add(new Employee(103, "Sohan", "NETWORK"));
 		list.add(new Employee(102, "Rohan", "NETWORK"));
+		System.out.println("Hello All ");
 
-		Collections.sort(list, new NameSorting());
-		System.out.println(list);
-		Collections.sort(list, new SalarySorting());
-		System.out.println(list);
 	}
 
 }
