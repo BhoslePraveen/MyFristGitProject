0a804e8bcc87eafd880d55e444cf6284923911f9
praveen <praveen.bhosle5@gmail.com>
2023-07-26 21:49:20 +0530

    added new lines

--- src/com/velocity/linkedlist/Demo1.java
+++ src/com/velocity/linkedlist/Demo1.java
@@ -11,10 +11,9 @@ public class Demo1 {
 		numberList.add(12);
 		numberList.add(15);
 		numberList.addFirst(11);
-		numberList.add(18);
-		numberList.add(12);
 		numberList.add(20);
 		numberList.removeLast();
+		numberList.addFirst(25);
 
 		ListIterator<Integer> itr = numberList.listIterator();
 		while (itr.hasNext()) {
